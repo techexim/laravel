597c763d5af87fae25f56fabe03a671fcb03184b
Long Do <me@dotronglong.com>
2015-12-08 17:59:24 +0700

    Fix bug

--- src/Support/HasSoftDeletes.php
+++ src/Support/HasSoftDeletes.php
@@ -1,15 +1,18 @@
 <?php namespace TechExim\Support;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 trait HasSoftDeletes
 {
+    use SoftDeletes;
+
     /**
      * Boot the soft deleting trait for a model.
      *
      * @return void
      */
-    public static function bootHasSoftDeletes()
+    public static function bootSoftDeletes()
     {
         if (static::$useSoftDeletes) {
             static::addGlobalScope(new SoftDeletingScope);
